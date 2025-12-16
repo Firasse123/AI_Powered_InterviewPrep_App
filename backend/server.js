2a3d400af79993c1fe1447f924f6010bd4c99161
Firasse123 <firasguizani2012@gmail.com>
2025-12-17 00:29:58 +0100

    Auth Controller and Auth Routes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express=require("express");
 const cors=require("cors");
 const path=require("path")
-
+const connectDB=require("./config/db");
+const authRoutes=require("./routes/authRoutes");
 const app=express();
 //add middleware to handle CORS 
 
@@ -15,10 +16,19 @@ app.use(cors({
 app.use(express.json());
 
 //Routes
+app.use("/api/auth",authRoutes);
+//app.use("/api/session",sessionRoutes);
+//app.use("/api/question",questionRoutes);
+
+//app.use("/api/ai/generate-questions",protect,generateInterviewQuestions);
+//app.use("/api/ai/generate-explanation",protect,generateExplanation);
 
 //Server uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
-
+app.get("/",(req,res)=>{
+    res.send("Hello from InterviewPrepAI backend server");
+})
+connectDB();
 //Start Server
 const PORT =process.env.PORT || 5000;
 app.listen(PORT,()=>{
